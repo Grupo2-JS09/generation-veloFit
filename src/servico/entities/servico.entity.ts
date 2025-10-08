30b98a4134f43cd1dcba7082c4188538a4467175
Anna Clara <andradeannac@gmail.com>
2025-10-08 10:51:19 -0300

    Conclusão modulo services

---
+++ src/servico/entities/servico.entity.ts
@@ -0,0 +1,24 @@
+import { isNotEmpty, IsNotEmpty } from "class-validator";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: "tb_servicos"})
+export class Servico{
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({type: "decimal", precision: 10, scale: 2})
+    @IsNotEmpty()
+    valor_mensalidade: number
+
+    @Column({nullable: false })
+    @IsNotEmpty()
+    frequencia: number
+
+    @Column()
+    @IsNotEmpty()
+    dt_matricula: Date
+
+    @Column({length: 250})
+    modalidade: string
+
+}
