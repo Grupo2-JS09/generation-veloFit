cc50e317e9a67f8b9cb19902c103a90855c007a0
Maristela Rocha <maristela.rocha.silva@gmail.com>
2025-10-08 09:05:05 -0300

    chore: Environment initial setup

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuario } from './usuario/entities/usuario.entity';
+import { UsuarioModule } from './usuario/usuario.module';
+import { Servico } from './servico/entities/servico.entity';
+import { ServicoModule } from './servico/servico.module';
+import { Module } from '@nestjs/common';
+import { Categoria } from './categoria/entities/categoria.entity';
+import { CategoriaModule } from './categoria/categoria.module';
+import { AuthModule } from './auth/auth.module';
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_veloFood',
+      entities: [Usuario, Servico, Categoria],
+      synchronize: false,
+      logging: true,
+    }),
+    UsuarioModule,
+    ServicoModule,
+    CategoriaModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
